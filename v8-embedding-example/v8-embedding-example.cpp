317d48042a85b1b3546e3fd8086aed33b123103e
David Erceg <david@explorerplusplus.com>
2019-03-07 21:39:17 +1100

    Add some basic code to run a JavaScript function using V8/v8pp

--- v8-embedding-example/v8-embedding-example.cpp
+++ v8-embedding-example/v8-embedding-example.cpp
@@ -1,21 +1,49 @@
-// v8-embedding-example.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
+#include "v8pp/context.hpp"
+#include <libplatform/libplatform.h>
+#include <v8.h>
+#include <PathCch.h>
 #include <iostream>
 
-int main()
+void runJavaScriptCode();
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n"; 
+	v8::V8::InitializeExternalStartupData(argv[0]);
+
+	auto platform = v8::platform::NewDefaultPlatform();
+	v8::V8::InitializePlatform(platform.get());
+	v8::V8::Initialize();
+
+	runJavaScriptCode();
+
+	v8::V8::Dispose();
+	v8::V8::ShutdownPlatform();
+
+	return EXIT_SUCCESS;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void runJavaScriptCode()
+{
+	v8pp::context context;
+	v8::HandleScope handleScope(context.isolate());
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	auto result = context.run_script(
+		"function fib(n) {"
+			"if (n == 0) {"
+				"return 0;"
+			"}"
+			"else if (n == 1) {"
+				"return 1;"
+			"}"
+
+			"return fib(n - 1) + fib(n - 2);"
+		"}"
+
+		"fib(8);"
+	);
+
+	v8::String::Utf8Value resultString(context.isolate(), result);
+
+	std::cout << "The result of the script was: " << *resultString << "\n";
+}
